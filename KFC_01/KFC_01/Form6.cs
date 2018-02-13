7f6b3520a23b62fad5cc2990d696197af5c358c8
Márton Gergő <sarbatorjanos@gmail.com>
2018-02-13 18:34:03 +0100

    Add files via upload

--- KFC_01/KFC_01/Form6.cs
+++ KFC_01/KFC_01/Form6.cs
@@ -20,7 +20,7 @@ namespace KFC_01
 
             string query = "SELECT name, clockin FROM LoggedIn";
 
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\gregg\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\gregg\Desktop\KFC\Data.mdf; Integrated Security = True; Connect Timeout = 30");
 
             DataTable dt = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter(query, con);
@@ -29,35 +29,10 @@ namespace KFC_01
             da.Fill(dt);
             con.Close();
 
-            //DataRow r1 = dt.Rows[0];
-            //DataRow r2 = dt.Rows[1];
-            ////label 1-20 name
-            ////label 21-40 clockin
-
-            //foreach (DataRow row in dt.Rows)
-            //{
-
-            //    label1.Text = r1["name"].ToString();
-            //    label4.Text = r1["clockin"].ToString();
-
-            //    label2.Text = r2["name"].ToString();
-            //    label5.Text = r2["clockin"].ToString();
-
-            //}
-
-            foreach (DataColumn col in dt.Columns)
-            {
-                foreach (DataRow row in dt.Rows)
-                {
-                    label1.Text = row["name"].ToString();
-                    label2.Text = row[col.ColumnName].ToString();
-                    label1.Text = row[col.ColumnName].ToString();
-                    label2.Text = row[col.ColumnName].ToString();
-                }
-            }
-
+            dataGridView1.DataSource = dt;
+            DataGridViewColumn column = dataGridView1.Columns[0];
+            column.Width = 200;
         }
-
         private void Form6_KeyDown(object sender, KeyEventArgs e)
         {
             this.TopMost = false;
